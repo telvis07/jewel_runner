f84be9502bda0b5ee1ef37a9cf4f5cb0c26b1fd3
Telvis Calhoun <telvis07@gmail.com>
2015-04-23 22:40:15 -0400

    made sample_rate configurable. fixed null pointer error with getResult

--- src/main/scala/jewel_runner/transcriber/SphinxTranscriberJsonResult.scala
+++ src/main/scala/jewel_runner/transcriber/SphinxTranscriberJsonResult.scala
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.databind.SerializationFeature
 
 import edu.cmu.sphinx.api._
+import edu.cmu.sphinx.result.WordResult
 import edu.cmu.sphinx.util.LogMath
 
 // TODO: What is this??
@@ -32,99 +33,68 @@ object SphinxTranscriberJsonResult {
 
   def main(args : Array[String]){
 
-    if (args.length != 1){
+    if (args.length == 0){
       System.err.println("Number of args: "+args.length.toString)
-      System.err.println("Usage : PROG <path_to_file>")
+      System.err.println("Usage : PROG <path_to_file> [sample_rate]")
       System.exit(1)
     }
 
     val inputFile = args(0)
+    var sample_rate = 8000
+    if (args.length == 2){
+      sample_rate = args(1).toInt
+    }
 
-    /*
-    // edu.cmu.sphinx.api.Configuration
-    // Configuration options for speech recognition
-    */
+    /* configuration */
     val configuration = new Configuration
-
-    /*
-    // Set path to acoustic model
-    // Feature vectors that map raw audio to phones
-    // An acoustic model maps to phones (or triphones)
-    //
-    // What is a triphone?
-    // first part of the phone depends on its preceding phone,
-    // the middle part is stable, and the next part depends on the subsequent phone.
-    // That's why there are often three states in a phone selected for speech recognition.
-    // For example “u with left phone b and right phone d” in the word “bad”
-    //
-    // Usually we use 4000 distinct short sound detectors to compose detectors for triphones
-    */
     configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us")
-
-    /*
-    // Set path to dictionary.
-    // A dictionary maps phones to words
-    // from cmudict-en-us.dict
-      one W AH N
-      two T UW
-      three TH R IY
-      five F AY V
-      seven S EH V AH N
-      twenty T W EH N T IY
-      twenty(2) T W EH N IY
-     */
     configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict")
-
-    /*
-    // Set language model.
-    // http://cmusphinx.sourceforge.net/doc/sphinx4/edu/cmu/sphinx/result/Lattice.html
-    //
-    // Graph that contains statistics of word sequences. It limits the search space by
-    // predicting the next word given prior words
-
-             "a"           "cat"
-           /     \        /     \
-       <s>          "big"         - </s>
-           \     /        \     /
-            "all"          "dog"
-     */
     configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.dmp")
+    configuration.setSampleRate(sample_rate)
 
+    /* Open the file */
     val stream = new FileInputStream(inputFile)
+
+    // The header of a WAV (RIFF) file is 44 bytes long
+    // http://www.topherlee.com/software/pcm-tut-wavformat.html
     stream.skip(44)
 
     // initialize the recognizer
     val recognizer : StreamSpeechRecognizer = new StreamSpeechRecognizer(configuration)
 
     // Simple recognition with generic model
-    // From: http://cmusphinx.sourceforge.net/wiki/tutorialconcepts
     recognizer.startRecognition(stream)
 
-    /*
-    // Decode audio (Recognizer.decode, Decoder.decode) -> Extract features (FrontEnd.getData),
-    // find phones, search language model (searchManager.recognize())
-    // calls Recognizer.decode ->  ->
-    // Features:
-    // http://cmusphinx.sourceforge.net/doc/sphinx4/edu/cmu/sphinx/frontend/FrontEnd.html
-    */
+    /* Get the first result */
     var result : SpeechResult = recognizer.getResult()
 
     while (result != null) {
-      val speech_record = new scala.collection.mutable.HashMap[String, Any]
+      // Sometimes the result can be non-null but the results are still empty
+      if (result.getHypothesis.length > 0){
+        val speech_record = new scala.collection.mutable.HashMap[String, Any]
+        speech_record("hypothesis") = result.getHypothesis
+        println("List of recognized words and their times:")
 
-      speech_record("hypothesis") = result.getHypothesis
-      println("List of recognized words and their times:")
+        val word_results = result.getWords
 
-      speech_record("words") = result.getWords.map(r => {
-        Map("word" -> r.getWord.toString,
-            "start" -> r.getTimeFrame.getStart.toString,
-            "end" -> r.getTimeFrame.getEnd.toString,
-            "confidence_log" -> LogMath.getLogMath.logToLinear(r.getConfidence.toFloat))
-      })
+        // Try to handle unexpected null values
+        speech_record("words") = word_results match {
+          case _ if (word_results == null) => Map()
+          case _ => word_results.map({ r  => {
+            Map("word" -> r.getWord.toString,
+              "start" -> r.getTimeFrame.getStart.toString,
+              "end" -> r.getTimeFrame.getEnd.toString,
+              "confidence_log" -> LogMath.getLogMath.logToLinear(r.getConfidence.toFloat))
+          }
+          })
+        }
 
-      speech_record("n_best") = result.getNbest(3)
-      println(to_json_string(speech_record))
-      println("calling recognizer.getResult()")
+        speech_record("n_best") = result.getNbest(3)
+        println(to_json_string(speech_record))
+        println("calling recognizer.getResult()")
+      }
+
+      // Get the next result
       result = recognizer.getResult()
     }
     recognizer.stopRecognition()
